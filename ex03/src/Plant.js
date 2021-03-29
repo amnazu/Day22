4977d7a0664acccfb6cf6d42a4973b5bf368c61c
amnazu <amna_zukic@hotmail.com>
2021-03-30 01:28:35 +0200

    Dvadeset drugi dan

---
+++ ex03/src/Plant.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+
+class Plant extends Component {
+    render() {
+        return (
+            <div className="Plant">
+                <h2>This is a Plant Component!</h2>
+            </div>
+        )
+    }
+}
+
+export default Plant; 
